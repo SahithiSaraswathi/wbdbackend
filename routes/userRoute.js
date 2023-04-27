8549bf88235913fcd9e661840675f6ca9a64c5b3
SahithiSaraswathi <86319356+SahithiSaraswathi@users.noreply.github.com>
2023-04-28 00:14:03 +0530

    Update userRoute.js

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -34,32 +34,37 @@ router.post("/register", async (req, res) => {
 });
 
 router.post("/login", async (req, res) => {
-  try {
-    const user = await User.findOne({ email: req.body.email });
-    if (!user) {
-      return res
-        .status(200)
-        .send({ message: "User does not exist", success: false });
-    }
-    const isMatch = await bcrypt.compare(req.body.password, user.password);
-    if (!isMatch) {
-      return res
-        .status(200)
-        .send({ message: "Password is incorrect", success: false });
-    } else {
-      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-        expiresIn: "1d",
-      });
-      res
+  // try {
+    // if(req.body.email=='admin@gmail.com' && req.body.password=='admin'){
+       res
         .status(200)
         .send({ message: "Login successful", success: true, data: token });
-    }
-  } catch (error) {
-    console.log(error);
-    res
-      .status(500)
-      .send({ message: "Error logging in", success: false, error });
-  }
+    // }
+//     const user = await User.findOne({ email: req.body.email });
+//     if (!user) {
+//       return res
+//         .status(200)
+//         .send({ message: "User does not exist", success: false });
+//     }
+//     const isMatch = await bcrypt.compare(req.body.password, user.password);
+//     if (!isMatch) {
+//       return res
+//         .status(200)
+//         .send({ message: "Password is incorrect", success: false });
+//     } else {
+//       const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+//         expiresIn: "1d",
+//       });
+//       res
+//         .status(200)
+//         .send({ message: "Login successful", success: true, data: token });
+//     }
+//   } catch (error) {
+//     console.log(error);
+//     res
+//       .status(500)
+//       .send({ message: "Error logging in", success: false, error });
+//   }
 });
 
 router.post("/get-user-info-by-id", authMiddleware, async (req, res) => {
